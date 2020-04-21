0f7d44591af1423e878a8da6b072b2cb6d614c2a
Daniel Mastropietro <mastropi@uwalumni.com>
2020-04-21 19:13:10 +0200

    Eliminated the test on the MJeremy2017 implementation which are no longer relevant

--- Python/test/test_TD_Lambda.py
+++ Python/test/test_TD_Lambda.py
@@ -14,8 +14,6 @@ import unittest
 #from gym.utils import seeding
 import matplotlib.pyplot as plt
 
-from Python.lib.GitHub.MJeremy2017.RL.RandomWalk_Lambda import TD_Lambda as TDL
-from Python.lib import TD_Lambda_DM as TDLDM
 from Python.lib.environments import gridworlds
 import Python.lib.agents as agents
 from Python.lib.agents.policies import random_walks
@@ -42,11 +40,6 @@ class Test_TD_Lambda(unittest.TestCase):
                             # (i.e. they belong to the "object" instantiated by this class)
         cls.plotFlag = True
 
-        # Setup based on the code from MJeremy2017 (which was a little modified)
-        cls.rw_MJDM = TDLDM.RandomWalk_DM(start=9, lmbda=0.8, debug=False)
-        cls.rw_td_MJDM = TDL.RWTD(start=9, debug=False)
-
-        # Using the MJeremy2017 definition refactored into separate concepts (env, policies, learners)
         cls.nS = 19             # Number of non-terminal states in the 1D gridworld
         cls.env = gridworlds.EnvGridworld1D(length=cls.nS+2)  # nS states plus the two terminal states
         # True state value functions
@@ -62,15 +55,6 @@ class Test_TD_Lambda(unittest.TestCase):
         cls.agent_rw_tdlambda = agents.GeneralAgent(cls.rw, cls.tdlambda)
         cls.agent_rw_tdlambda_adaptive = agents.GeneralAgent(cls.rw, cls.tdlambda_adaptive)
         cls.agent_rw_mclambda = agents.GeneralAgent(cls.rw, cls.mclambda)
-
-    def setUp(self):
-        # Make the tests repeatable
-        # NOTE: This setUp() is run before EVERY test, as there is setUpClass() that is run
-        # just once before ALL the tests are run.
-        #self.random_generator, seed = seeding.np_random(self.seed)
-        # This is used by the MJeremy2017 implementation (since they call np.random.choice() to choose the action)
-        # (as opposed to seeding.np_random() which is the case in the DiscreteEnv environment of gym --which I use)
-        np.random.seed(self.seed)
 
     def plot_results(self, V_estimated, V_true, RMSE_by_episode, plotFlag):
         if plotFlag:
@@ -90,44 +74,6 @@ class Test_TD_Lambda(unittest.TestCase):
             ax.set_title(self.id())
 
     #------------------------------------------- TESTS ----------------------------------------
-    def test_random_walk_result_MJDM(self):
-        "Test using the off-line learning (that calls gt2tn() implemented in TD_Lambda_DM.py"
-        print("\nTesting " + self.id())
-        valueFunc = TDLDM.ValueFunction_DM(alpha=0.2)
-        self.rw_MJDM.play(valueFunc, rounds=self.nrounds)
-
-        expected = np.array([ 0.,         -0.97026362, -1.09293459, -0.9594995,  -0.8038009,  -0.74118132,
- -0.70110327, -0.5745329,  -0.38799777, -0.12274256, -0.03652859,  0.16429707,
-  0.37958913,  0.43674974,  0.46251896,  0.60353857,  0.83647189,  0.94198936,
-  0.95498279,  0.84075131,  0.        ])
-# DM-2020/04/06: Previous result, when an action was spared at the end of each episode because
-# it was taken BEFORE going to the next iteration (as opposed to take it soon after the new iteration starts)
-#                [ 0.,         -0.99498334, -1.12844023, -1.08742679, -0.99925887, -0.92014644,
-# -0.83281915, -0.73404135, -0.67868899, -0.54605007, -0.32858876,  0.09982778,
-#  0.14763525,  0.19785301,  0.33526039,  0.59847646,  0.66730606,  0.86347524,
-#  0.99748532,  0.95018769,  0.        ])
-        observed = valueFunc.getValues()
-
-        print("\nobserved: " + str(observed))
-
-        assert np.allclose( expected, observed )
-
-    def test_random_walk_td_result_MJDM(self):
-        "Test using MJeremy2017' TD(Lambda) learner"
-        print("\nTesting " + self.id())
-        valueFunc = TDLDM.ValueFunctionTD(alpha=0.2, gamma=0.9, lmbda=0.8)
-        self.rw_td_MJDM.play(valueFunc, rounds=self.nrounds)
-
-        expected = np.array([ 0.,         -0.93600092, -0.62865005, -0.32600362, -0.23004461, -0.20544315,
- -0.17822048, -0.1183389,  -0.04995117, -0.01169715, -0.00284312,  0.02066435,
-  0.05085406,  0.05865383, 0.07223799,  0.15062597,  0.32170616,  0.43413232,
-  0.49881925,  0.73350279, 0.        ])
-        observed = valueFunc.getValues()
-
-        print("\nobserved: " + str(observed))
-
-        assert np.allclose( expected, observed )
-
     def test_random_walk_result(self):
         "Test using my TD(Lambda) learner"
         print("\nTesting " + self.id())
