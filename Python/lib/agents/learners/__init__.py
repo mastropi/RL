Fixed bugs in the setting of lambda = 0 and in the computation of RMSE by episode
The bugs were:
- lambda: when the lambda value passed to the learner constructor was 0, the value of lambda to use in the simulation (self.lambda) was NOT set but instead set to its default value of 0.8.
The reason is that the IF condition I was using: `lmbda if lmbda else self.lmbda` resolves to False when lmbda = 0! (the correct condition is `if lmbda is not None`.
- RMSE: I was computing RMSE at the end of the episode in two different places: in simulate() and in play(). I was using the one in play() to store the RMSE by episode returned to the outside world, which was computed as an average *weighted* by the state occupation count. This gives a higher value than the non-weigthed RMSE since the mid states are the most visited ones and also the ones which usually show higher error.

Other changes:
- Learners: new parameter `adjust_alpha_by_episode` was added in order to decrease alpha by episode number instead of by state occupation count --which seems to give a very aggressive decrease, making the algorithm converge very slowly to the true value function.
- mc.py: new methods learn_pred_V_mc() and learn_mc() that apply directly the MC learning algorithm used to check whether the lambda-return algorithm gives the same results as the MC method when lambda = 1.
- EnvironmentDiscrete class: new attribute `terminal_states` that store the states that are terminal in the environment.
- Added results of the simulation on TD(0) and MC presented in the presentation `202004Apr30-ResultsTDLambda&MC-1DGridworld.pptx` that I sent to Urtzi and Matt on 02-May-2020.

Python/lib/agents/learners/__init__.py
@@ -41,7 +41,7 @@ import numpy as np
 from Python.lib.environments import EnvironmentDiscrete
 
 
-MIN_COUNT = 10      # Minimum state count to start shrinking alpha
+MIN_COUNT = 30      # Minimum state count to start shrinking alpha
 MIN_EPISODE = 10    # Minimum episode count to start shrinking alpha
 MAX_EPISODE_FOR_ALPHA_MIN = None  # Maximum episode on which the minimum alpha value above is applied
 
@@ -58,18 +58,19 @@ class Learner:
     the first simulation as opposed to the correct value 1.
     """
 
-    def __init__(self, env, alpha, adjust_alpha=False, alpha_min=0.):
+    def __init__(self, env, alpha, adjust_alpha=False, adjust_alpha_by_episode=True, alpha_min=0.):
         """
         @param env: environment
         @param alpha: learning rate
         """
-        if not isinstance(env, EnvironmentDiscrete):
-            raise TypeError("The environment must be of type {} from the {} module ({})" \
-                            .format(EnvironmentDiscrete.__name__, EnvironmentDiscrete.__module__, env.__class__))
+#        if not isinstance(env, EnvironmentDiscrete):
+#            raise TypeError("The environment must be of type {} from the {} module ({})" \
+#                            .format(EnvironmentDiscrete.__name__, EnvironmentDiscrete.__module__, env.__class__))
 
         self.env = env
         self.alpha = alpha
         self.adjust_alpha = adjust_alpha
+        self.adjust_alpha_by_episode = adjust_alpha_by_episode
         self.alpha_min = alpha_min          # Used when adjust_alpha=True
 
         # Information of the observed trajectory at the END of the episode
@@ -79,6 +80,8 @@ class Learner:
 
         # Episode counter
         self.episode = 0
+        # (Average) alpha used at each episode
+        self.alpha_mean_by_episode = []
 
         # State counts over ALL episodes run after reset
         self._state_counts_overall = np.zeros(self.env.getNumStates())
@@ -96,6 +99,7 @@ class Learner:
         """
         if reset_episode:
             self.episode = 0
+            self.alpha_mean_by_episode = []
             # State counts over ALL episodes run after reset
             self._state_counts_overall = np.zeros(self.env.getNumStates())
             # Learning rate for each state (so that we can reduce alpha with time)
@@ -106,6 +110,9 @@ class Learner:
         # Increase episode counter
         # (note that the very first episode run is #1 because reset() is called by __init__()) 
         self.episode += 1
+        # List of alphas used during the episode (which may vary from state to state and from the time visited)
+        self._alphas_used_in_episode = []
+
         # Reset the attributes that keep track of states and rewards received during learning
         self._reset_at_start_of_episode()
 
@@ -114,10 +121,11 @@ class Learner:
         if self.episode == 1 or reset_value_functions:
             self.V.reset()
 
-    def setParams(self, alpha, adjust_alpha, alpha_min):
-        self.alpha = alpha if alpha else self.alpha
-        self.adjust_alpha = adjust_alpha if adjust_alpha else self.adjust_alpha
-        self.alpha_min = alpha_min if alpha_min else self.alpha_min
+    def setParams(self, alpha, adjust_alpha, adjust_alpha_by_episode, alpha_min):
+        self.alpha = alpha if alpha is not None else self.alpha
+        self.adjust_alpha = adjust_alpha if adjust_alpha is not None else self.adjust_alpha
+        self.adjust_alpha_by_episode = adjust_alpha_by_episode if adjust_alpha_by_episode is not None else self.adjust_alpha_by_episode
+        self.alpha_min = alpha_min if alpha_min is not None else self.alpha_min
 
     def _reset_at_start_of_episode(self):
         """
@@ -145,23 +153,32 @@ class Learner:
 
     def _update_alphas(self, state):
         if self.adjust_alpha:
-            # Update using the state occupation counter over all past episodes 
-            if MAX_EPISODE_FOR_ALPHA_MIN is None or self.episode <= MAX_EPISODE_FOR_ALPHA_MIN:
-                # This means we should apply the alpha_min value indefinitely or up to the max episode specified by MAX_EPISODE_FOR_ALPHA_MIN
-                self._alphas[state] =  np.max([ self.alpha_min, self.alpha / np.max([1, self._state_counts_overall[state] - MIN_COUNT + 2]) ])
-                if MAX_EPISODE_FOR_ALPHA_MIN is not None and self.episode == MAX_EPISODE_FOR_ALPHA_MIN:
-                    # Store the last alpha value observed for each state
-                    # so that we can use it as starting point from now on.
-                    self._alphas_at_max_episode = self._alphas.copy()
-                    #print("episode {}, state {}: alphas at max episode: {}".format(self.episode, state, self._alphas_at_max_episode))
+            if self.adjust_alpha_by_episode:
+                # Update using the episode number (equal for all states)
+                self._alphas[state] =  np.max([ self.alpha_min, self.alpha / np.max([1, self.episode - MIN_EPISODE + 1]) ])
             else:
-                # Start decreasing from the alpha value left at episode = MAX_EPISODE_FOR_ALPHA_MIN
-                # without any lower bound for alpha
-                self._alphas[state] = self._alphas_at_max_episode[state] / np.max([1, self._state_counts_overall[state] - MIN_COUNT + 2])
-                #print("episode {}, state {}: alphas: {}".format(self.episode, state, self._alphas))
-    
-            # Update using the episode number (equal for all states)
-            #self._alphas[state] =  np.max([ self.alpha_min, self.alpha / np.max([1, self.episode - MIN_EPISODE + 1]) ])
+                # Update using the state occupation count over all past episodes 
+                if MAX_EPISODE_FOR_ALPHA_MIN is None or self.episode <= MAX_EPISODE_FOR_ALPHA_MIN:
+                    # This means we should apply the alpha_min value indefinitely or
+                    # up to the max episode specified by MAX_EPISODE_FOR_ALPHA_MIN
+                    self._alphas[state] =  np.max([ self.alpha_min, self.alpha / np.max([1, self._state_counts_overall[state] - MIN_COUNT + 2]) ])
+                    if MAX_EPISODE_FOR_ALPHA_MIN is not None and self.episode == MAX_EPISODE_FOR_ALPHA_MIN:
+                        # Store the last alpha value observed for each state
+                        # so that we can use it as starting point from now on.
+                        self._alphas_at_max_episode = self._alphas.copy()
+                        #print("episode {}, state {}: alphas at max episode: {}".format(self.episode, state, self._alphas_at_max_episode))
+                else:
+                    # Start decreasing from the alpha value left at episode = MAX_EPISODE_FOR_ALPHA_MIN
+                    # without any lower bound for alpha
+                    self._alphas[state] = self._alphas_at_max_episode[state] / np.max([1, self._state_counts_overall[state] - MIN_COUNT + 2])
+                    #print("episode {}, state {}: alphas: {}".format(self.episode, state, self._alphas))
+
+        assert set([state]).issubset( set(self.env.terminal_states) ) == False, \
+                "The state on which the alpha is computed is NOT a terminal state"
+        self._alphas_used_in_episode += [self._alphas[state]]
+
+        #print("episode {}, state {}: count={:.0f}, alphas >= {}: {}     {}" \
+        #      .format(self.episode, state, self._state_counts_overall[state], self.alpha_min, self._alphas[state], self._alphas))
 
     def store_trajectory(self):
         "Stores the trajectory observed during the episode"
@@ -174,6 +191,12 @@ class Learner:
 
     def final_report(self, T):
         "Processes to be run at the end of the learning process"
+        # Store the (average) learning rate used during the episode for the new episode just ended
+        # Note: Using the average is only relevant when the adjustment is by state occupation count,
+        # NOT when it is by episode since in the latter case all the alphas are the same
+        # for all states visited during the episode.
+        self.alpha_mean_by_episode += [np.mean(self._alphas_used_in_episode)]
+
         if self.debug:
             self.plot_alphas(T)
 

Python/lib/agents/learners/mc.py
@@ -22,8 +22,10 @@ class LeaMCLambda(Learner):
         env (gym.envs.toy_text.discrete.DiscreteEnv): the environment where the learning takes place.
     """
 
-    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8, adjust_alpha=False, alpha_min=0., debug=False):
-        super().__init__(env, alpha, adjust_alpha, alpha_min)
+    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8,
+                 adjust_alpha=False, adjust_alpha_by_episode=True, alpha_min=0.,
+                 debug=False):
+        super().__init__(env, alpha, adjust_alpha, adjust_alpha_by_episode, alpha_min)
         self.debug = debug
 
         # Attributes that MUST be presented for all TD methods
@@ -58,10 +60,10 @@ class LeaMCLambda(Learner):
         # lambda-return (G(t,lambda))
         self._Glambda_list = []
 
-    def setParams(self, alpha=None, gamma=None, lmbda=None, adjust_alpha=None, alpha_min=0.):
-        super().setParams(alpha, adjust_alpha, alpha_min)
-        self.gamma = gamma if gamma else self.gamma
-        self.lmbda = lmbda if lmbda else self.lmbda
+    def setParams(self, alpha=None, gamma=None, lmbda=None, adjust_alpha=None, adjust_alpha_by_episode=None, alpha_min=0.):
+        super().setParams(alpha, adjust_alpha, adjust_alpha_by_episode, alpha_min)
+        self.gamma = gamma if gamma is not None else self.gamma
+        self.lmbda = lmbda if lmbda is not None else self.lmbda
 
     def learn_pred_V_slow(self, t, state, action, next_state, reward, done, info):
         # This learner updates the estimate of the value function V ONLY at the end of the episode
@@ -125,6 +127,25 @@ class LeaMCLambda(Learner):
             G += self.gamma**(end - start) * self.getV().getValue(self._states[end])
 
         return G
+
+    def learn_pred_V_mc(self, t, state, action, next_state, reward, done, info):
+        "Learn the prediction problem (estimate the state value function) using explicitly MC"
+        self._update_alphas(state)
+        self._update_trajectory(state, reward)
+
+        if done:
+            # Store the trajectory and rewards
+            self.store_trajectory()
+
+            # This means t+1 is the terminal time T
+            # (recall we WERE in time t and we STEPPED INTO time t+1, so T = t+1)
+            self.learn_mc(t)
+
+            # Reset the internal attributes used during learning
+            # (but NOT the value functions estimations, since we want to learn always more at each episode!)
+            self._reset_at_start_of_episode()
+
+            self.final_report(t)
 
     def learn_pred_V(self, t, state, action, next_state, reward, done, info):
         "Learn the prediction problem: estimate the state value function"
@@ -200,6 +221,23 @@ class LeaMCLambda(Learner):
             if self.debug:
                 print("[DONE] t: {} \tG(t:t+n): {} \n\tG(t,lambda): {}".format(t, self._G_list, self._Glambda_list)) 
 
+    def learn_mc(self, t):
+        "Updates the value function based on the new observed episode using first-visity Monte Carlo"
+        # Terminal time
+        T = t + 1
+
+        #-- Compute the observed return for each state in the trajectory for EVERY visit to it
+        # NOTE: we start at the LATEST state (as opposed to the first) so that we don'tt
+        # need to have a data structure that stores the already visited states in the episode;
+        # we trade data structure creation and maintenance with easier algorithmic implementation of
+        # first visit that does NOT require a special data structure storage.
+        G = 0
+        for tt in np.arange(T,0,-1) - 1:     # This is T-1, T-2, ..., 0
+            state = self._states[tt]
+            G = self.gamma*G + self._rewards[tt+1]
+            delta = G - self.V.getValue(state)
+            self.updateV(state, delta)
+
     def learn(self, t):
         "Updates the value function based on the new observed episode"
         # Terminal time
@@ -234,8 +272,10 @@ class LeaMCLambda(Learner):
 
 class LeaMCLambdaAdaptive(LeaMCLambda):
     
-    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8, adjust_alpha=False, debug=False):
-        super().__init__(env, alpha, gamma, lmbda, adjust_alpha, debug)
+    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8,
+                 adjust_alpha=False, adjust_alpha_by_episode=True, alpha_min=0.,
+                 debug=False):
+        super().__init__(env, alpha, gamma, lmbda, adjust_alpha, adjust_alpha_by_episode, alpha_min, debug)
 
         # Arrays that keep track of previous _rewards for each state
         self.all_states = np.arange(self.env.getNumStates())

Python/lib/agents/learners/td.py
@@ -21,8 +21,10 @@ class LeaTDLambda(Learner):
         env (gym.envs.toy_text.discrete.DiscreteEnv): the environment where the learning takes place.
     """
 
-    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8, adjust_alpha=False, alpha_min=0., debug=False):
-        super().__init__(env, alpha, adjust_alpha, alpha_min)
+    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8,
+                 adjust_alpha=False, adjust_alpha_by_episode=True, alpha_min=0.,
+                 debug=False):
+        super().__init__(env, alpha, adjust_alpha, adjust_alpha_by_episode, alpha_min)
         self.debug = debug
 
         # Attributes that MUST be presented for all TD methods
@@ -42,10 +44,10 @@ class LeaTDLambda(Learner):
         self._z[:] = 0.
         self._z_all = np.zeros((0,self.env.getNumStates()))
 
-    def setParams(self, alpha=None, gamma=None, lmbda=None, adjust_alpha=None, alpha_min=0.):
-        super().setParams(alpha, adjust_alpha, alpha_min)
-        self.gamma = gamma if gamma else self.gamma
-        self.lmbda = lmbda if lmbda else self.lmbda
+    def setParams(self, alpha=None, gamma=None, lmbda=None, adjust_alpha=None, adjust_alpha_by_episode=None, alpha_min=0.):
+        super().setParams(alpha, adjust_alpha, adjust_alpha_by_episode, alpha_min)
+        self.gamma = gamma if gamma is not None else self.gamma
+        self.lmbda = lmbda if lmbda is not None else self.lmbda
 
     def learn_pred_V(self, t, state, action, next_state, reward, done, info):
         self._update_trajectory(state, reward)
@@ -80,9 +82,10 @@ class LeaTDLambda(Learner):
 
 class LeaTDLambdaAdaptive(LeaTDLambda):
     
-    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8, adjust_alpha=False, alpha_min=0.,
+    def __init__(self, env, alpha=0.1, gamma=0.9, lmbda=0.8,
+                 adjust_alpha=False, adjust_alpha_by_episode=True, alpha_min=0.,
                  lambda_min=0., burnin=False, debug=False):
-        super().__init__(env, alpha, gamma, lmbda, adjust_alpha, alpha_min, debug)
+        super().__init__(env, alpha, gamma, lmbda, adjust_alpha, adjust_alpha_by_episode, alpha_min, debug)
         # Minimum lambda for the adaptive lambda so that there is still some impact
         # in past states at the beginning when all state values are equal and equal to 0
         self.lambda_min = lambda_min
@@ -96,11 +99,11 @@ class LeaTDLambdaAdaptive(LeaTDLambda):
         # bootstrapping information about the value function at the next state)  
         self.state_counts_noreset = np.zeros(self.env.getNumStates())
 
-    def setParams(self, alpha=None, gamma=None, lmbda=None, adjust_alpha=None, alpha_min=0.,
+    def setParams(self, alpha=None, gamma=None, lmbda=None, adjust_alpha=None, adjust_alpha_by_episode=None, alpha_min=0.,
                   lambda_min=0., burnin=False):
-        super().setParams(alpha, gamma, lmbda, adjust_alpha, alpha_min)
-        self.lambda_min = lambda_min if lambda_min else self.lambda_min
-        self.burnin = burnin if burnin else self.burnin
+        super().setParams(alpha, gamma, lmbda, adjust_alpha, adjust_alpha_by_episode, alpha_min)
+        self.lambda_min = lambda_min if lambda_min is not None else self.lambda_min
+        self.burnin = burnin if burnin is not None else self.burnin
 
     def learn_pred_V(self, t, state, action, next_state, reward, done, info):
         self._update_trajectory(state, reward)

Python/lib/environments/__init__.py
@@ -17,18 +17,20 @@ class EnvironmentDiscrete(discrete.DiscreteEnv):
     """
     Class defining methods that are generic to ALL environments.
     """
-    def __init__(self, nS, nA, P, isd):
+    def __init__(self, nS, nA, P, isd, terminal_states=set()):
         """
         Calls the constructor of the super class which requires:
         - nS: number of possible states
         - nA: number of possible actions
         - P: probability of each state-action pair
         - isd: initial state distribution
+        - terminal_states: set containing the terminal states of the environment
         """
         super().__init__(nS, nA, P, isd)
 
         # All states
         self.all_states = np.arange(self.getNumStates())
+        self.terminal_states = terminal_states
 
     #--- Getters
     def getInitialStateDistribution(self):
@@ -47,6 +49,9 @@ class EnvironmentDiscrete(discrete.DiscreteEnv):
     def getNumStates(self):
         # Same comment as for getNumActions() and super()
         return self.nS
+
+    def getTerminalStates(self):
+        return self.terminal_states
 
     #--- Setters
     def setInitialStateDistribution(self, isd):

Python/lib/simulators.py
@@ -61,7 +61,7 @@ class Simulator:
     def __exit__(self, exc_type, exc_val, exc_tb):
         # Restore the initial state distribution array in case it has been possibly changed
         # (e.g. when we want to define a specific initial state)
-        if self._isd_orig:
+        if self._isd_orig is not None:
             self.setInitialStateDistribution(self._isd_orig)
 
     def reset(self):
@@ -75,7 +75,9 @@ class Simulator:
         # Reset the learner to the first episode state
         self.agent.getLearner().reset(reset_episode=True, reset_value_functions=True)
 
-    def play(self, nrounds, start=None, seed=None, compute_rmse=False, plot=False, colormap="seismic", pause=0):
+    def play(self, nrounds, start=None, seed=None, compute_rmse=False,
+             verbose=False, verbose_period=1,
+             plot=False, colormap="seismic", pause=0):
         # TODO: (2020/04/11) Convert the plotting parameters to a dictionary named plot_options or similar.
         # The goal is to group OPTIONAL parameters by their function/concept.  
         """Runs an episodic Reinforcement Learning experiment.
@@ -101,6 +103,12 @@ class Simulator:
             Whether to compute the RMSE over states (weighted by their number of visits)
             after each episode. Useful to analyze rate of convergence of the estimates.
 
+        verbose: bool, optional
+            Whether to show the experiment that is being run and the episodes for each experiment.
+            
+        verbose_period: int, optional
+            Every how many episodes per experiment should be displayed.
+
         plot: bool, optional
             Whether to generate plots showing the evolution of the value function estimates.
 
@@ -114,12 +122,15 @@ class Simulator:
             of the value function estimates.
 
         Returns: tuple
-            Tuple containing the following elements having the same size as the environment states:
-                - state value function estimates
-                - number of visits at each state
+            Tuple containing the following elements:
+                - state value function estimate for each state at the end of the episode (`nrounds`)
+                - number of visits to each state at the end of the episode
                 - RMSE (when compute_rmse is not None) an array of length `nrounds` containing the
                 Root Mean Square Error after each episode, of the estimated value function averaged
-                over all states and weighted by the number of visits to each state. Otherwise, None.
+                over all states. Otherwise, None.
+                - a dictionary containing additional relevant information, as follows:
+                    - 'alphas': the value of the learning parameter `alpha` for each state
+                    at the end of the episode.
         """
         if plot:
             fig_V = plt.figure()
@@ -128,7 +139,7 @@ class Simulator:
             plt.plot(self.env.all_states, self.env.getV(), '.-', color="blue")
 
         # Define initial state
-        if start:
+        if start is not None:
             nS = self.env.getNumStates()
             if not (isinstance(start, int) and 0 <= start and start < nS):
                 raise Warning('The `start` parameter must be an integer number between 0 and {}.' \
@@ -155,8 +166,9 @@ class Simulator:
             self.env.reset()
             learner.reset(reset_episode=False, reset_value_functions=False)
             done = False
+            if verbose and np.mod(episode, verbose_period) == 0:
+                print("Episode {} of {} running...".format(episode+1, nrounds))
             if self.debug:
-                print("\n\nEpisode {} starts...".format(episode+1))
                 print("\nStarts at state {}".format(self.env.getState()))
                 print("\tState value function at start of episode:\n\t{}".format(learner.getV().getValues()))
 
@@ -186,7 +198,7 @@ class Simulator:
 
             if compute_rmse:
                 if self.env.getV() is not None:
-                    RMSE[episode] = rmse(self.env.getV(), learner.getV().getValues(), weights=self.state_counts)
+                    RMSE[episode] = rmse(self.env.getV(), learner.getV().getValues())#, weights=self.state_counts)
 
             if plot:
                 #print("episode: {} (T={}), color: {}".format(episode, t, colors(episode/nrounds)))
@@ -206,13 +218,18 @@ class Simulator:
             plt.sca(ax) # Go back to the primary axis
             #plt.figure(fig_V.number)
 
-        # TODO: DO WE NEED THIS RESTORE OF THE INITIAL STATE DISTRIBUTION OR THE __exit__() METHOD SUFFICES??
-#        if self.isd_orig:
-#            self.env.setInitialStateDistribution(self.isd_orig)
+        # Restore the initial state distribution of the environment (for the next simulation)
+        # Note that the restore step done in the __exit__() method may NOT be enough, because the Simulator object
+        # may still exist once the simulation is over.
+        if self._isd_orig is not None:
+            self.env.setInitialStateDistribution(self._isd_orig)
+            self._isd_orig = None
 
-        return learner.getV().getValues(), self.state_counts, RMSE
+        return  learner.getV().getValues(), self.state_counts, RMSE, \
+                {'alphas_at_episode_end': learner._alphas,  # value of parameter alpha for each state at the end of the episode
+                 'alphas_by_episode': learner.alpha_mean_by_episode}   # (Average) alpha by episode run so far (averaged over visited states if relevant)  
 
-    def simulate(self, nexperiments, nepisodes, start=None, verbose=False):
+    def simulate(self, nexperiments, nepisodes, start=None, verbose=False, verbose_period=1):
         """Simulates the agent interacting with the environment for a number of experiments and number
         of episodes per experiment.
         
@@ -228,12 +245,22 @@ class Simulator:
             When None, the starting state is picked randomly following the initial state distribution
             of the environment.
 
+        verbose: bool, optional
+            Whether to show the experiment that is being run and the episodes for each experiment.
+            
+        verbose_period: int, optional
+            Every how many episodes per experiment should be displayed.
+
         Returns: tuple
             Tuple containing the following elements:
-                Avg(RMSE): Root Mean Square Error averaged over all experiments
-                SE(RMSE): Standard Error of the average RMSE
-                Episodic RMSE: array containing the Root Mean Square Error by episode averaged
+                - Avg(N): The average number of visits to each state over all experiments  
+                - Avg(RMSE): Root Mean Square Error averaged over all experiments
+                - SE(RMSE): Standard Error of the average RMSE
+                - Episodic RMSE: array containing the Root Mean Square Error by episode averaged
                 over all experiments.
+                - a dictionary containing additional relevant information, as follows:
+                    - 'alphas': the value of the learning parameter `alpha` for each state
+                    at the end of the episode in the last experiment.
         """
     
         if not (isinstance(nexperiments, int) and nexperiments > 0):
@@ -241,9 +268,10 @@ class Simulator:
         if not (isinstance(nepisodes, int) and nepisodes > 0):
             raise ValueError("The number of episodes must be a positive integer number ({})".format(nepisodes))
 
-        RMSE = 0.
-        RMSE2 = 0.  # Used to compute the standard error of the average RMSE
-        RMSE_by_episodes = np.zeros(nepisodes)
+        N = 0.      # Average number of visits to each state over all experiments
+        RMSE = 0.   # RMSE at the end of the experiment averaged over all experiments
+        RMSE2 = 0.  # Used to compute the standard error of the RMSE averaged over all experiments
+        RMSE_by_episodes = np.zeros(nepisodes)  # RMSE at each episode averaged over all experiments
         RMSE_by_episodes2 = np.zeros(nepisodes) # Used to compute the standard error of the RMSE by episode
 
         # IMPORTANT: We should use the seed of the environment and NOT another seed setting mechanism
@@ -252,20 +280,25 @@ class Simulator:
         # This seed is then used for the environment evolution realized with the reset() and step() methods.
         [seed] = self.env.seed(self.seed)
         for exp in np.arange(nexperiments):
+            self.agent.getLearner().reset(reset_episode=True, reset_value_functions=True)
             if verbose:
                 print("Running experiment {} of {} (#episodes = {})..." \
                       .format(exp+1, nexperiments, nepisodes), end=" ")
-            self.agent.getLearner().reset(reset_episode=True, reset_value_functions=True)
-            V, N, RMSE_by_episodes_i = self.play(nrounds=nepisodes, start=start, seed=None,
-                                                 compute_rmse=True, plot=False)
-            RMSE_i = rmse(self.env.getV(), V) #, weights=N)
+                print("Value function at start of experiment: {}".format(self.agent.getLearner().getV().getValues()))
+            V, N_i, RMSE_by_episodes_i, learning_info = self.play(nrounds=nepisodes, start=start, seed=None,
+                                                                  compute_rmse=True, plot=False,
+                                                                  verbose=verbose, verbose_period=verbose_period)
+            N += N_i
+            # RMSE at the end of the last episode
+            RMSE_i = RMSE_by_episodes_i[-1]
             RMSE += RMSE_i
             RMSE2 += RMSE_i**2
             RMSE_by_episodes += RMSE_by_episodes_i
             RMSE_by_episodes2 += RMSE_by_episodes_i**2
             if verbose:
-                print("\tRMSE(experiment) = {:.3g}".format(RMSE_i))
+                print("\tRMSE(at end of experiment) = {:.3g}".format(RMSE_i))
 
+        N_mean = N / nexperiments
         RMSE_mean = RMSE / nexperiments
         RMSE_se = np.sqrt( ( RMSE2 - nexperiments*RMSE_mean**2 ) / (nexperiments - 1) ) \
                 / np.sqrt(nexperiments)
@@ -274,7 +307,7 @@ class Simulator:
         RMSE_by_episodes_se = np.sqrt( ( RMSE_by_episodes2 - nexperiments*RMSE_by_episodes_mean**2 ) / (nexperiments - 1) ) \
                             / np.sqrt(nexperiments)
 
-        return RMSE_mean, RMSE_se, RMSE_by_episodes_mean, RMSE_by_episodes_se
+        return N_mean, RMSE_mean, RMSE_se, RMSE_by_episodes_mean, RMSE_by_episodes_se, learning_info
 
 
 # TODO: (2020/04/11) Move this function to an util module
@@ -317,7 +350,8 @@ if __name__ == "__main__":
     colormap = cm.get_cmap("jet")
     max_alpha = 1
     max_rmse = 0.5
-    fig, (ax_full, ax_scaled, ax_rmse_by_episode) = plt.subplots(1,3)
+    #fig, (ax_full, ax_scaled, ax_rmse_by_episode) = plt.subplots(1,3)
+    fig, (ax_full, ax_scaled) = plt.subplots(1,2)
 
     # The environment
     env = gridworlds.EnvGridworld1D(length=21)
@@ -333,8 +367,8 @@ if __name__ == "__main__":
 
     # Simulation setup
     seed = 1717
-    nexperiments = 2
-    nepisodes = 5
+    nexperiments = 1
+    nepisodes = 10
     start = None
     useGrid = False
     verbose = True
@@ -349,19 +383,12 @@ if __name__ == "__main__":
         alphas = np.linspace(0.1, 0.7, n_alphas)
     else:
         lambdas = [0, 0.4, 0.8, 0.9, 0.95, 0.99, 1]
-        alphas = np.linspace(0.1, 1.2, 5)
-        #lambdas = [0.99]
+        alphas = np.linspace(0.1, 0.9, 8)
+        lambdas = [0, 0.4, 0.7, 0.8, 0.9]
         #alphas = [0.41]
         n_lambdas = len(lambdas)
         n_alphas = len(alphas)
     n_simul = n_lambdas*n_alphas
-
-    #learner.setParams(alpha=0.41, gamma=gamma, lmbda=0.1)
-    #agent = GeneralAgent(pol_rw,
-    #                     learner)
-    #sim = Simulator(env, agent)
-    #rmse_mean, rmse_se = sim.simulate(nexperiments=nexperiments, nepisodes=nepisodes, verbose=verbose)
-    #print(rmse_mean, rmse_se)
 
     # List of dictionaries, each containing the characteristic of each parameterization considered
     results_list = []
@@ -388,6 +415,7 @@ if __name__ == "__main__":
             
             # Reset learner and agent (i.e. erase all memory from a previous run!)
             learner.setParams(alpha=alpha, gamma=gamma, lmbda=lmbda)
+            learner.reset(reset_episode=True, reset_value_functions=True)
             agent = GeneralAgent(pol_rw,
                                  learner)
             # NOTE: Setting the seed here implies that each set of experiments
@@ -399,10 +427,11 @@ if __name__ == "__main__":
             sim = Simulator(env, agent, seed=seed, debug=debug)
 
             # Run the simulation and store the results
-            rmse_mean, rmse_se, rmse_episodes = sim.simulate(nexperiments=nexperiments,
-                                                             nepisodes=nepisodes,
-                                                             start=start,
-                                                             verbose=verbose)
+            N_mean, rmse_mean, rmse_se, rmse_episodes, _, learning_info = \
+                                sim.simulate(nexperiments=nexperiments,
+                                             nepisodes=nepisodes,
+                                             start=start,
+                                             verbose=verbose)
             results_list += [{'lmbda': lmbda,
                               'alpha': alpha,
                               'rmse': rmse_mean,
@@ -423,9 +452,17 @@ if __name__ == "__main__":
         rmse_episodes_se[idx_lmbda] = np.array(rmse_episodes_se_lambda)
 
         # Plot the average RMSE for the current lambda as a function of alpha
-        color = colormap( idx_lmbda / np.max((1, n_lambdas-1)) )
-        ax_full.plot(alphas, rmse_mean_lambda, '.', color=color)
-        ax_full.errorbar(alphas, rmse_mean_lambda, yerr=rmse_se_lambda, capsize=4, color=color)
+        #rmse2plot = rmse_mean_lambda
+        #rmse2plot_error = rmse_se_lambda
+        #ylabel = "Average RMSE over all {} states, at the end of episode {}, averaged over {} experiments".format(env.getNumStates(), nepisodes, nexperiments)
+        rmse2plot = rmse_episodes_mean_lambda
+        rmse2plot_error = rmse_episodes_se_lambda
+        ylabel = "Average RMSE over all {} states, first {} episodes, and {} experiments".format(env.getNumStates(), nepisodes, nexperiments)
+
+        # Map blue to the largest lambda and red to the smallest lambda (most similar to the color scheme used in Sutton, pag. 295)
+        color = colormap( 1 - idx_lmbda / np.max((1, n_lambdas-1)) )
+        ax_full.plot(alphas, rmse2plot, '.', color=color)
+        ax_full.errorbar(alphas, rmse2plot, yerr=rmse2plot_error, capsize=4, color=color)
         legend_label += ["lam={:.2g}".format(lmbda)]
 
     # Average RMSE by episode for convergence analysis
@@ -444,8 +481,10 @@ if __name__ == "__main__":
     for idx_lmbda, lmbda in enumerate(lambdas):
         #rmse2plot = rmse_mean_values[idx_lmbda]
         #rmse2plot_error = rmse_se_values[idx_lmbda]
+        #ylabel = "Average RMSE over all {} states, at the end of episode {}, averaged over {} experiments".format(env.getNumStates(), nepisodes, nexperiments)
         rmse2plot = rmse_episodes_mean[idx_lmbda]
         rmse2plot_error = rmse_episodes_se[idx_lmbda]
+        ylabel = "Average RMSE over all {} states, first {} episodes, and {} experiments".format(env.getNumStates(), nepisodes, nexperiments)
         color = colormap( 1 - idx_lmbda / np.max((1, n_lambdas-1)) )
         ax_scaled.plot(alphas, rmse2plot, '.-', color=color)
         ax_scaled.errorbar(alphas, rmse2plot, yerr=rmse2plot_error, capsize=4, color=color)
@@ -453,11 +492,11 @@ if __name__ == "__main__":
         ax_scaled.set_ylim((0, max_rmse))
 
     # Episodic RMSE
-    ax_rmse_by_episode.plot(np.arange(nepisodes), rmse_episodes_values, color="black")
-    ax_rmse_by_episode.set_ylim((0, max_rmse))
-    ax_rmse_by_episode.set_xlabel("Episode")
-    ax_rmse_by_episode.set_ylabel("RMSE")
-    ax_rmse_by_episode.set_title("Average RMSE by episode over ALL experiments")
+    #ax_rmse_by_episode.plot(np.arange(nepisodes), rmse_episodes_values, color="black")
+    #ax_rmse_by_episode.set_ylim((0, max_rmse))
+    #ax_rmse_by_episode.set_xlabel("Episode")
+    #ax_rmse_by_episode.set_ylabel("RMSE")
+    #ax_rmse_by_episode.set_title("Average RMSE by episode over ALL experiments")
 
     plt.figlegend(legend_label)
     fig.suptitle("{}: gamma = {:.2g}, #experiments = {}, #episodes = {}"\