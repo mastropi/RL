406725e8cb7fd7d7a7a7b96597ffed0d7aa1598e
Daniel Mastropietro <mastropi@uwalumni.com>
2022-06-23 14:57:59 +0200

    [Est] Used the data_provider class to make testing of the FV probability estimation SCALABLE
    DETAILS:
    - test_estimators.py:
      - Test_EstimatorQueueBlockingFlemingViot class: a set of 3 tests are now run on the usual single-server system with rho = 0.7, one for small K (5), another for moderate K (20), and another for large K (40).
      This is achieved by defining the queue system and FV execution parameters in a lambda function used by the data_provider class defined in the unittest_data_provider module.

--- Python/test/test_estimators.py
+++ Python/test/test_estimators.py
@@ -14,6 +14,7 @@ import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 import unittest
+from unittest_data_provider import data_provider
 
 from Python.lib import estimators
 from Python.lib import simulators
@@ -117,29 +118,26 @@ class Test_EstimatorQueueBlockingFlemingViot(unittest.TestCase):
     def setUpClass(cls):
         # Environment 1: single server queue with Poisson arrivals and departures
         nservers = 1
-        capacity = 20 #5
         rates_birth = [0.7]
         rates_death = [1.0]
-        queue_mm_single_server = queues.QueueMM(rates_birth, rates_death, nservers, capacity, origin=0.0)
-        # We consider there is only one job arriving at the single server queue
-        job_class_rates = queue_mm_single_server.getBirthRates()
-        reward_func = None
-        rewards_accept_by_job_class = [1.0]
-        dict_params_reward_func = None
-        cls.env_queue_mm_single_server = queues.EnvQueueSingleBufferWithJobClasses( queue_mm_single_server,
-                                                                                    job_class_rates,
-                                                                                    reward_func,
-                                                                                    rewards_accept_by_job_class,
-                                                                                    dict_params_reward_func)
-
-        # Execution parameters that do not define the system
-        cls.exec_params = { 'J': int(capacity/2),     # Size of the absorption set
-                            'N': 100, #5,                   # Number of particles
-                            'T': 2000, #20,                  # Number of arrival events
-                            }
+        capacities = [5, 20, 40]
+        dict_queue_mm_single_server = dict()
+        cls.dict_env_queue_mm_single_server = dict()
+        for idx_K, K in enumerate(capacities):
+            dict_queue_mm_single_server[K] = queues.QueueMM(rates_birth, rates_death, nservers, capacities[idx_K], origin=0.0)
+            # We consider there is only one job type arriving at the single server queue
+            job_class_rates = dict_queue_mm_single_server[K].getBirthRates()
+            reward_func = None
+            rewards_accept_by_job_class = [1.0]
+            dict_params_reward_func = None
+            cls.dict_env_queue_mm_single_server[K] = queues.EnvQueueSingleBufferWithJobClasses( dict_queue_mm_single_server[K],
+                                                                                                job_class_rates,
+                                                                                                reward_func,
+                                                                                                rewards_accept_by_job_class,
+                                                                                                dict_params_reward_func)
 
     @measure_exec_time
-    def run_fv_estimation(self, env_queue, J, N, T, seed=1717):
+    def run_fv_estimation(self, env_queue, K, J, N, T, seed=1717):
         """
         Test the Fleming-Viot estimation of the blocking probability of a queue
 
@@ -161,51 +159,81 @@ class Test_EstimatorQueueBlockingFlemingViot(unittest.TestCase):
             Seed to use for the pseudo-random number generation.
         """
         print("\n--> Running Fleming-Viot estimation on a single server system...")
-        # Environment
+
+        # Queue environments to use as FV particles
         envs_queue = [env_queue if i == 0 else copy.deepcopy(env_queue) for i in range(N)]
+
         # Agent interacting with the environment (which defines whether to accept or reject a job)
         # Define the agent acting on the queue environment
         # The agent blocks an incoming job when the buffer size of the queue is at its capacity K.
         # This is achieved by setting the parameter theta of the parameterized linear step acceptance policy
         # to the integer value K-1.
-        K = self.env_queue_mm_single_server.getCapacity()
         policies = dict({QueuePolicyTypes.ACCEPT: PolQueueTwoActionsLinearStep(env_queue,
                                                                                float(K-1)),
                          QueuePolicyTypes.ASSIGN: None})
-        learners = dict({LearnerTypes.V: LeaFV(self.env_queue_mm_single_server, gamma=1.0),
+        learners = dict({LearnerTypes.V: LeaFV(env_queue, gamma=1.0),
                          LearnerTypes.Q: None,
                          LearnerTypes.P: None})
         agent_accept_reject = AgeQueue(env_queue, policies, learners)
+
+        # Simulation parameters
         dict_params_simul = dict({'buffer_size_activation': J,  # J-1 is the absorption buffer size
                                   'T': T,                       # number of arrivals at which the simulation stops
                                   'seed': seed})
         dict_params_info = dict()
 
+        # Run the simulation!
         return simulators.estimate_blocking_fv( envs_queue, agent_accept_reject,
                                                 dict_params_simul, dict_params_info)
 
-    def test_fv_implementation_single_server(self):
+    #--------------------- TESTS OF MC: traditional MC (no lambda involved) ----------------------#
+    #-------- DATA -------
+    # Case number, description, expected value, parameters
+    # These are the same tests 1, 2 and 3 from data_test_lambda_return_random_walk
+    data_test_fv_implementation_single_server = lambda: (
+            (1, 'Small K',
+                {'K': 5, 'J': 2, 'N': 5, 'T': 20},
+                {'Pr(FV)': 0.0598497, 'E(T)': 5.70, '#E(T)': 8, 'Tmax': 45.6, 'Tend': 50.3, 'Smax': 5.6, '#events_ET': 40, '#events_FV': 47}),
+            (2, 'Moderate K',
+                {'K': 20, 'J': 10, 'N': 100, 'T': 2000},
+                {'Pr(FV)': 0.00040698, 'E(T)': 40.14, '#E(T)': 70, 'Tmax': 2809.5, 'Tend': 2840.9, 'Smax': 37.1, '#events_ET': 4003, '#events_FV': 6370}),
+            (3, 'Large K',
+                {'K': 40, 'J': 35, 'N': 400, 'T': 2000},
+                {'Pr(FV)': 0.0059430, 'E(T)': 6.09, '#E(T)': 1, 'Tmax': 6.1, 'Tend': 2849.5, 'Smax': 3.2,
+                 '#events_ET': 4033, '#events_FV': 2239}),
+    )
+    #-------- DATA -------
+
+    @data_provider(data_test_fv_implementation_single_server)
+    def test_fv_implementation_single_server(self, casenum, desc, dict_params, dict_expected):
         "Test the Fleming-Viot implementation of the blocking probability of a single-server queue system"
-        nservers = self.env_queue_mm_single_server.getNumServers()
-        rates_job = self.env_queue_mm_single_server.getJobClassRates()
-        rates_service = self.env_queue_mm_single_server.getServiceRates()
-        K = self.env_queue_mm_single_server.getCapacity()
-        J = self.exec_params['J']
-        N = self.exec_params['N']
-        T = self.exec_params['T']
+        print("\n*** Testing {}, case number {}: '{}' ***".format(self.id(), casenum, desc))
+        K = dict_params['K']
+        nservers = self.dict_env_queue_mm_single_server[K].getNumServers()
+        rates_job = self.dict_env_queue_mm_single_server[K].getJobClassRates()
+        rates_service = self.dict_env_queue_mm_single_server[K].getServiceRates()
+        #K = self.env_queue_mm_single_server.getCapacity()
+        #J = self.exec_params['J']
+        #N = self.exec_params['N']
+        #T = self.exec_params['T']
         proba_blocking_fv, expected_reward, probas_stationary, \
             expected_absorption_time, n_absorption_time_observations, \
-                time_last_absorption, time_end_simulation_et, max_survival_time, \
-                    n_events_et, n_events_fv_only = self.run_fv_estimation(self.env_queue_mm_single_server, J, N, T, seed=1313)
+                time_last_absorption, time_end_simulation_et, max_survival_time, time_end_simulation_fv, \
+                    n_events_et, n_events_fv_only = self.run_fv_estimation(self.dict_env_queue_mm_single_server[K],
+                                                                           dict_params['K'],
+                                                                           dict_params['J'],
+                                                                           dict_params['N'],
+                                                                           dict_params['T'],
+                                                                           seed=1313)
 
         print("EXECUTION PARAMETERS:")
         print("- # servers: {}".format(nservers))
         print("- job arrival rates at servers: {}".format(rates_job))
         print("- service rates: {}".format(rates_service))
         print("- capacity: {}".format(K))
-        print("- absorption set size: {}".format(J))
-        print("- # particles: {}".format(N))
-        print("- # arrival time steps: {}".format(T))
+        print("- absorption set size: {}".format(dict_params['J']))
+        print("- # particles: {}".format(dict_params['N']))
+        print("- # arrival time steps: {}".format(dict_params['T']))
         print("")
 
         print("ESTIMATION RESULTS:")
@@ -217,42 +245,36 @@ class Test_EstimatorQueueBlockingFlemingViot(unittest.TestCase):
         print("- Time of last absorption = {:.1f}".format(time_last_absorption))
         print("- Last observed time of the MC simulation for E(T) = {:.1f}".format(time_end_simulation_et))
         print("- Max observed survival time = {:.1f}".format(max_survival_time))
+        print("- Time FV simulation ends = {:.1f}".format(time_end_simulation_fv))
         print("- Number of events in MC simulation for E(T) = {}".format(n_events_et))
         print("- Number of events in FV simulation for Phi(t) = {}".format(n_events_fv_only))
 
-        # Check results
-        if nservers == 1 and K == 5 and rates_job == [0.7] and rates_service == [1.0] and \
-            J == int(K/2) and N == 5 and T == 20:
-            self.assertAlmostEqual(proba_blocking_fv, 0.0598497)
-            self.assertAlmostEqual(expected_absorption_time, 5.704832, places=6)    # places = decimal places
-            self.assertEqual(n_absorption_time_observations, 8)
-            self.assertAlmostEqual(time_last_absorption, 45.6, places=1)
-            self.assertAlmostEqual(time_end_simulation_et, 50.3, places=1)
-            self.assertAlmostEqual(max_survival_time, 5.6, places=1)
-            self.assertEqual(n_events_et, 40)
-            self.assertEqual(n_events_fv_only, 47)
-            # Execution time is about 0.15-0.20 sec (when manually averaged over 4-5 runs on a not charged PC --i.e. recently restarted)
-        elif nservers == 1 and K == 20 and rates_job == [0.7] and rates_service == [1.0] and \
-            J == int(K/2) and N == 100 and T == 2000:
-            self.assertAlmostEqual(proba_blocking_fv, 0.00040698)
-            self.assertAlmostEqual(expected_absorption_time, 40.14, places=2)    # places = decimal places
-            self.assertEqual(n_absorption_time_observations, 70)
-            self.assertAlmostEqual(time_last_absorption, 2809.5, places=1)
-            self.assertAlmostEqual(time_end_simulation_et, 2840.9, places=1)
-            self.assertAlmostEqual(max_survival_time, 37.1, places=1)
-            self.assertEqual(n_events_et, 4003)
-            self.assertEqual(n_events_fv_only, 6370)
-        else:
-            raise warnings.warn("No assertion has been executed. Check that the execution parameters fall into one of the asserted cases!")
+        # Check system setup is the one required to obtain the expected results
+        assert nservers == 1, "Number of servers is 1"
+        assert rates_job == [0.7], "Arrival rate is 0.7"
+        assert rates_service == [1.0], "Service rate is 1.0"
+
+        # Assertions
+        self.assertAlmostEqual(proba_blocking_fv, dict_expected['Pr(FV)'])
+        self.assertAlmostEqual(expected_absorption_time, dict_expected['E(T)'], places=2)  # places = decimal places
+        self.assertEqual(n_absorption_time_observations, dict_expected['#E(T)'])
+        self.assertAlmostEqual(time_last_absorption, dict_expected['Tmax'], places=1)
+        self.assertAlmostEqual(time_end_simulation_et, dict_expected['Tend'], places=1)
+        self.assertAlmostEqual(max_survival_time, dict_expected['Smax'], places=1)
+        self.assertEqual(n_events_et, dict_expected['#events_ET'], 40)
+        self.assertEqual(n_events_fv_only, dict_expected['#events_FV'], 47)
+        # Consistency assertions
+        self.assertAlmostEqual(proba_blocking_fv, probas_stationary[K])
+        self.assertAlmostEqual(time_end_simulation_fv, max_survival_time, places=1)
 
 
 if __name__ == '__main__':
     # Reference for creating test suites:
     # https://stackoverflow.com/questions/15971735/running-single-test-from-unittest-testcase-via-command-line
 
-    tests2run = ["value_function"]
+    #tests2run = ["value_function"]
     #tests2run = ["value_function", "fv"]
-    #tests2run = ["fv"]
+    tests2run = ["fv"]
 
     runner = unittest.TextTestRunner()
 
