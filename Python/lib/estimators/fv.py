[FV] Fixed bug in the calculation of the FV integral for the discounted case (gamma < 1) in compute_fv_integral()
The bug was that the factor (1 - gamma^(dt+1)) was used inside the sum, instead of the correct (1 - gamma^dt). Note that the correctness of the latter factor is confirmed by the recovery of the FV discounted summation formula in two border cases: dt = 0 and dt = 1, which before was NOT recovered.

In addition to this, the value of the reward was taken out of the sum() expression to make computation faster.

Finally, in LeaFV, fixed a small bug in the _update_integral() method where I was NOT computing the integral update by including the reward at the state of interest x (assumed a terminal state). Note that this bug did NOT have any effect so far because all non-zero rewards considered in my tests were always equal to +1.

Python/lib/estimators/fv.py
@@ -118,7 +118,7 @@ def empirical_mean(envs: list, state: Union[list, int]):
 
 def update_phi(env, N: int, t: float, dict_phi: dict, env_state_prev, env_state_cur):
     """
-    Updates the conditional probability Phi of each state of interest, which are stored by the keys
+    Updates the conditional probability Phi of each state of interest at the given time t, which are stored by the keys
     of the dictionary of input parameter dict_phi
 
     Arguments:
@@ -516,21 +516,24 @@ def compute_fv_integral(df_phi_proba_surv, reward: float=1.0, interval_size: flo
     #   but here dt = 0 (which we filter below) because there is no next value to use for the computation of dt (which, recall, is computed as "next t" - "current t".
     # - The value of dt is 0 when several occurrences of the same t happen when observing the survival times of the process
     # (e.g. when working on a discrete-time Markov process), and at the last record inserted in the data frame (which contains the maximum survival time observed so far).
-    df_phi_proba_surv_Phi_gt0 = df_phi_proba_surv.loc[(df_phi_proba_surv['Phi'] > 0) & (df_phi_proba_surv['dt'] > 0),]
+    df_phi_proba_surv_Phi_gt0 = df_phi_proba_surv.loc[(df_phi_proba_surv['Phi'] > 0) & (df_phi_proba_surv['dt'] > 0), :]
     if discount_factor == 1.0:
-        integral = interval_size * np.sum(reward * df_phi_proba_surv_Phi_gt0['P(T>t)'] * df_phi_proba_surv_Phi_gt0['Phi'] * df_phi_proba_surv_Phi_gt0['dt'])
+        integral = interval_size * reward * np.sum(df_phi_proba_surv_Phi_gt0['P(T>t)'] * df_phi_proba_surv_Phi_gt0['Phi'] * df_phi_proba_surv_Phi_gt0['dt'])
     else:
         # We need to consider each one-step-sized (i.e. dt = 1) interval separately, even if the function P(T>t)*Phi(t,x) is constant in there,
         # because we need to apply a different discount for each of those intervals.
         # Fortunately we do NOT need to explicitly iterate on all the one-step-sized intervals, because we can use the summation formula for a geometric sum
         # on the discount factor, and the computation of this geometric sum is possible because all the necessary ingredients are available in the input data frame!
         # The expression is:
-        #   Integral = interval_size * 1 / (1 - gamma) * sum_i { f(i) * gamma^(t(i) - 1) * (1 - gamma^(dt(i) + 1)) }
+        #   Integral = interval_size * 1 / (1 - gamma) * sum_i { f(i) * gamma^(t(i) - 1) * (1 - gamma^dt(i)) }
         # where i indexes every interval where f(i) remains constant, t(i) is the LOWER end of such interval,
         # and f(i) = P(T>t(i)) * Phi(t(i),x) and dt(i) is the length of such interval measured as t(i+1) - t(i).
-        integral = interval_size * 1 / (1 - discount_factor) * \
-                                    np.sum( discount_factor**(df_phi_proba_surv_Phi_gt0['t'] - 1) * (1 - discount_factor**(df_phi_proba_surv_Phi_gt0['dt'] + 1)) *
-                                            reward * df_phi_proba_surv_Phi_gt0['P(T>t)'] * df_phi_proba_surv_Phi_gt0['Phi'] )
+        # Note that extreme cases make sense:
+        # - when dt(i) = 0, the contribution to the sum is 0 because the factor (1 - gamma^dt(i)) becomes 0, and this makes total sense because the width of the interval is 0!
+        # - when dt(i) = 1 for all i, we recover the original expression as sum_i{ f(i) * gamma^(t(i) - 1) }.
+        integral = interval_size * reward * 1 / (1 - discount_factor) * \
+                                    np.sum( discount_factor**(df_phi_proba_surv_Phi_gt0['t'] - 1) * (1 - discount_factor**(df_phi_proba_surv_Phi_gt0['dt'])) *
+                                            df_phi_proba_surv_Phi_gt0['P(T>t)'] * df_phi_proba_surv_Phi_gt0['Phi'] )
 
     return integral
 

Python/test/test_FV.py
@@ -137,10 +137,10 @@ class Test_Class_LeaFV_discretetime(unittest.TestCase):
 
             # Assert the value of the integral
             for x in learner_fv.states_of_interest:
-                df_proba_surv = merge_proba_survival_and_phi(df_surv, learner_fv.dict_phi[x])
-                proba, integral = estimate_proba_stationary(df_proba_surv, 1.0)
-                #print(f"Components of the integral:\n{df_proba_surv}")
-                #print(f"Integral computed after the fact: {integral}")
+                df_proba_surv_and_phi = merge_proba_survival_and_phi(df_surv, learner_fv.dict_phi[x])
+                proba, integral = estimate_proba_stationary(df_proba_surv_and_phi, 1.0)
+                print(f"Components of the integral:\n{df_proba_surv_and_phi}")
+                print(f"Integral computed after the fact: {integral}")
 
                 print(f"Observed integral[x={x}]: {learner_fv.dict_integral[x]}")
                 assert np.isclose(learner_fv.dict_integral[x], integral), "The iterative method and the after the fact method must give the same value for the FV integral"