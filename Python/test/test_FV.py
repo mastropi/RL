[FV] Fixed iterative calculation of the FV integral in _run_simulation_fv()
Bug: In _run_simulation_fv(), the iterative calculation of the FV integral was NOT taking into account the contribution from the non-absorbed particles at the end of the simulation.

This is a partial commit so there may be a few things that do not work correctly. However, all 51 unit tests except two tests on the discrete-time FV estimation of differential value functions present in test_estimators_discretetime.py should pass. These FV tests fail because the expected result do not take into account the *iterative* estimation of the average reward that has now been implemented in _run_simulation_fv() of the discrete.Simulator class. I don't update the expected results of those tests now because I will do so when the other pieces of the FV simulation and estimation process have been committed, namely: (i) starting the E(T_A) simulation at a state of the activation set (as opposed to at a state of the absorption set), (ii) selecting the start state of the FV simulation following the exit state distribution estimated by the E(T_A) excursion.

DETAILS:
- learners/episodic/discrete/fv.py:
  - New method update_integral() which puts together all the three steps needed to iteratively update the FV integral (carried out by private methods) as a new absorption event is observed.

  - _update_integral() (private method): new parameter `fixed_sample_size=True` so that the update of the integral can also be done in the case where the sample size on which survival times are observed is known in advance, e.g. the number of particles N.

  - Renamed _update_absorption_times() --> _update_absorption_times_and_phi() to make explicit that Phi(t, x) is also updated by the method. In fact, a subtle change was performed in the value of time 't' added as last record to the dict_phi dictionary in order to avoid a rare bug that was observed when the following two situations happened at the same time: (i) there are 

Python/test/test_FV.py
@@ -33,10 +33,11 @@ class Test_Class_LeaFV_discretetime(unittest.TestCase):
         print("\n*** Running test " + self.id() + " ***")
 
         # Define the FV learner
+        N = 5
         absorption_set = set(range(2))
         absorption_boundary = max(absorption_set)
         activation_set = {absorption_boundary + 1}
-        learner_fv = LeaFV(self.env, N=5, T=10, absorption_set=absorption_set, activation_set=activation_set, TIME_RESOLUTION=1)
+        learner_fv = LeaFV(self.env, N=N, T=10, absorption_set=absorption_set, activation_set=activation_set) #, TIME_RESOLUTION=1) # This was commented out because the default value of TIME_RESOLUTION is supposed to be 1 and by omitting its value here we want to stress this out, as normally when creating the LeaFV object, we will forget about setting this parameter... This value of TIME_RESOLUTION=1 should we set for discrete-time Markov chains.
 
         # Create the particles and mock where they are so that we can have a Phi estimate
         envs = [copy.deepcopy(self.env) for _ in range(learner_fv.N)]
@@ -119,36 +120,46 @@ class Test_Class_LeaFV_discretetime(unittest.TestCase):
                             print(f"--> Particle p={p} reactivated to state s={state}")
                         print("----------------------")
                         print(f"Processing absorption time t_abs={t}...")
-                        learner_fv._update_absorption_times(t)
-                        learner_fv._update_phi_contribution(t)
-                        learner_fv._update_integral()
-                    learner_fv._update_phi(t, state_prev, state)
+                        learner_fv.update_integral(t, fixed_sample_size=True)
+                    learner_fv.update_phi(t, state_prev, state)
 
                     if t == t_max:
                         done = True
 
+            # Kill the particles that have not been absorbed (censored particles) at t=t_max, so that we can consider the contribution
+            n_particles_not_absorbed = N - len(absorption_times)
+
+            # Update the integral taking into account this last absorption of all remaining particles, which makes the estimated survival probability at t_max be equal to 0.
+            # NOTE that we only need to update the integral ONCE, even when more than one particle has yet to be absorbed.
+            learner_fv.update_integral(t, fixed_sample_size=True)
+
             # ASSERTIONS
             # Compute the after the fact survival probability (i.e. after all absorption times are observed)
             # and check the correctness of the iteratively computed integral of P(T>t)*Phi(t,x) for each state of interest x
-            df_surv = pd.DataFrame({'t': [0] + absorption_times,
-                                    'P(T>t)': [ n /len(absorption_times) for n in range(len(absorption_times), -1, -1)]})
+            _absorption_times_including_censoring = absorption_times + list(np.repeat(t_max, n_particles_not_absorbed))
+            df_surv = pd.DataFrame({'t': [0] + _absorption_times_including_censoring,
+                                    'P(T>t)': [ n /len(_absorption_times_including_censoring) for n in range(len(_absorption_times_including_censoring), -1, -1)]})
 
             print(f"\n--- Seed {seed} ({idx_seed + 1} of {len(seeds)}):")
 
-            # Assert the value of the integral
+            # Assert the value of Phi and of the integral, and check that the iteratively computed integral gives the same result as the integral computed in one shot at the end
             for x in learner_fv.states_of_interest:
-                df_proba_surv_and_phi = merge_proba_survival_and_phi(df_surv, learner_fv.dict_phi[x])
-                proba, integral = estimate_proba_stationary(df_proba_surv_and_phi, 1.0)
-                print(f"Components of the integral:\n{df_proba_surv_and_phi}")
-                print(f"Integral computed after the fact: {integral}")
-
-                print(f"Observed integral[x={x}]: {learner_fv.dict_integral[x]}")
-                assert np.isclose(learner_fv.dict_integral[x], integral), "The iterative method and the after the fact method must give the same value for the FV integral"
-
-            # Assert the value of Phi(t,x) for each seed
-            for x in learner_fv.states_of_interest:
+                print(f"CHECK Phi[x={x}]")
                 print(f"Observed Phi[x={x}]:\n{learner_fv.dict_phi[x]}")
                 assert np.allclose(learner_fv.dict_phi[x], dict_expected[seed]['dict_phi'])
+                print("(OK)")
+
+                # Compute the integral
+                print(f"\nCHECK Integral[x={x}]")
+                df_proba_surv_and_phi = merge_proba_survival_and_phi(df_surv, learner_fv.dict_phi[x])
+                _, integral = estimate_proba_stationary(df_proba_surv_and_phi, 1.0) # We pass a dummy expected reabsorption time value of 1.0 as we are not interested in checking the stationary probability value
+                print(f"Components of the integral:\n{df_proba_surv_and_phi}")
+                print(f"One-shot computed Integral[x={x}]: {integral}")
+
+                print(f"Iteratively computed Integral[x={x}]: {learner_fv.dict_integral[x]}")
+                assert np.isclose(learner_fv.dict_integral[x], integral), "The iterative method and the after the fact method must give the same value for the FV integral"
+                print("(OK)")
+
 
     def test_computation_of_phi_as_function_of_start_state_and_action(self):
         "Tests the calculation of Phi(t,x) for different start states s using mocked particle trajectories"
@@ -194,7 +205,7 @@ class Test_Class_LeaFV_discretetime(unittest.TestCase):
                 print(f"Particle #{pp}:\n{_particle.getTrajectory()}")
 
         # Update the value of Phi(t,x) for each state of interest x defined in the FV learner (which by default is the set of terminal states of the environment)
-        learner_fv._update_phi_function(particles)
+        learner_fv.update_phi_function(particles)
         for x in learner_fv.states_of_interest:
             print(f"\n*** Phi(t, x={x})***")
             for s in sorted(learner_fv.dict_phi_for_state.keys()):
@@ -362,7 +373,7 @@ if __name__ == '__main__':
                 print(f"Particle #{pp}:\n{_particle.getTrajectory()}")
 
         # Update the value of Phi(t,x) for each state of interest x defined in the FV learner (which by default is the set of terminal states of the environment)
-        learner_fv._update_phi_function(particles)
+        learner_fv.update_phi_function(particles)
         for x in learner_fv.states_of_interest:
             print(f"\n*** Phi(t, x={x})***")
             for s in sorted(learner_fv.dict_phi_for_state.keys()):

Python/test/test_optimizers_discretetime.py
@@ -96,13 +96,25 @@ class Test_EstPolicy_EnvGridworldsWithObstacles(unittest.TestCase):
                 if sadj is not None and sadj not in set.union(absorption_set, obstacles_set):
                     activation_set.add(sadj)
 
-        # Set of start states (the lower-left cell, if not given)
+        # Set of start states, embedded in the environment below as part of the initial state distribution set as the uniform distribution on these states
+        # When None, the set of start states is defined as:
+        # - The lower-left cell if the set is not given and no absorption set has been specified (although this is never the case because of the above definition of absorption_set,
+        # but still we leave the condition in the logic below in case we decide to change this behaviour later on).
+        # - A set of states in the FV active set. The precise set of states used depends on the learning criterion:
+        #   - for AVERAGE learning criterion: it is defined as the set of activation states, i.e. the outside boundary of the absorption set A, so that the TD learner and the FV
+        #   learner have a fair comparison.
+        #   NOTE that, in this case, this set of start states is used to start the single Markov chain excursion used to estimate the expected reabsorption time E(T_A).
+        #   And this is OK (as opposed to e.g. start such excursion INSIDE the absorption set A), because we first need to enter the absorption set A in order to measure the
+        #   reabsorption cycle time. What is questionable, however, is whether we should start the single Markov chain excursion at a state chosen UNIFORMLY at random in the
+        #   outside boundary of A or, instead, the state should be chosen following the exit state distribution.
+        #   - for all other learning criteria (e.g. DISCOUNTED learning criterion): it is defined as ALL states in the FV ACTIVE set
+        #   (N.B. *not* "activATION" set, which is usually much smaller than the ACTIVE set).
         if start_states_set is None:
-            if absorption_set is None:  # ALTHOUGH ABOVE WE DEFINE THE absorption_set EVEN WHEN IT IS GIVEN AS None BY THE USER (so, this IF condition will never be satisfied --still we leave it for reference, and in case we decide to change this behaviour later on)
+            if absorption_set is None:
                 # The set of start states is defined by just one state, namely the bottom-left of the environment, which is considered to be the entrance to the labyrinth
                 start_states_set = set(np.ravel_multi_index((env_shape[0]-1, 0), env_shape))
             else:
-                # We set the start state to the same set of states used by the FV learning so that TD and FV are more fairly compared)
+                # We set the start state to the same set of states used by the FV learning so that TD and FV are more fairly compared
                 if learning_criterion == LearningCriterion.AVERAGE:
                     # The start state is defined as any state in the activation set of the FV learning process (i.e. the outside boundary of the absorption set)
                     start_states_set = set(activation_set)

Python/test/tests.py
@@ -720,7 +720,7 @@ test_ac.setUpClass(shape=env_shape, nn_input=InputLayer.ONEHOT, nn_hidden_layer_
                     N=200, #50 #20 #100
                     T=1000, #1000, #3000,  # np.prod(env_shape) * 10  #100 #1000
                     obstacles_set=obstacles_set, absorption_set=absorption_set,
-                    start_states_set=None, #{nS-1}, #None,
+                    start_states_set={entry_state}, #{nS-1}, #None,
                     reset_method_value_functions=ResetMethod.ALLZEROS,
                     estimate_on_fixed_sample_size=True,
                     seed=seed, debug=False)